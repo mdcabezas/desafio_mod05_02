3ad6c6293a60d1cca8ede1db6ae792751407f0b0
Manuel Cabezas <manuelcabezas@gmail.com>
2023-03-08 23:38:37 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './styles.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./views/Home";
+import Favoritos from "./views/Favoritos";
+
+function App() {
+  const endpoint = "/fotos.json";
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favoritos" element={<Favoritos />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
